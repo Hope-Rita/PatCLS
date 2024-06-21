c14884f45f610947938631e4ab8dc57b461e9921
BUAA_zoutao <46260424+Hope-Rita@users.noreply.github.com>
2024-06-21 22:50:06 +0800

    Update Hierarchy_Emb.py

--- Model/Hierarchy_Emb.py
+++ Model/Hierarchy_Emb.py
@@ -39,7 +39,7 @@ class Hierarchy_Embedding(nn.Module):
         self.numbers = numbers
         self.hierarchy_tree = [tree.to(device) for tree in hierarchy_tree]
         self.tree_layers = len(hierarchy_tree)
-        self.clusters = self.rawMatrix()  # correlation across IPC codes in the certain level
+        self.clusters = self.rawMatrix()  # 同类之间的关系
 
         self.NodeEmb = nn.ModuleList(
             [nn.Embedding(embedding_dim=self.hidden_dim, num_embeddings=number) for number in numbers])
@@ -67,20 +67,34 @@ class Hierarchy_Embedding(nn.Module):
     def rawMatrix(self):
         clusters = []
         for hierarchy in self.hierarchy_tree:
+            n1, n2 = hierarchy.shape
+            adj_eye = torch.eye(n1).to(self.device)
+            # cluster = torch.matmul(torch.matmul(hierarchy.transpose(0, 1), adj_eye), hierarchy)  # n2 * n2
             cluster = torch.matmul(hierarchy.transpose(0, 1), hierarchy)  # n2 * n2
+
             clusters.append(cluster)
+            # print(nor_cluster.shape)
 
         return clusters
 
     def calculateF(self, embed_self, embed_up=None, embed_down=None, self_adj=None, hierarchy_low=None,
                    msk_mechanism=False, hierarchy_high=None, layer=0):
+        # h = torch.mm(embed_self, self.transform[layer].weight.t())
+        # ###################Verision 1###################
+        # h = embed_self
+        # mat_self = torch.mm(h, h.t()).masked_fill(self_adj != 1, -np.inf)
+        # msk_self = torch.softmax(mat_self, dim=1)
+        # aggred_mat = torch.mm(msk_self, h)
+        # return aggred_mat
+        # ###################Verision 1###################
         # high level
         h = embed_self
+        # aggred_mat = torch.mm(self_adj, h)
         adp_a = torch.mm(h, h.t())
         if msk_mechanism:
             adp_a = adp_a.masked_fill(self_adj != 1, -np.inf)
         soft_e = torch.softmax(adp_a, dim=1)
-        aggred_mat = torch.mm(soft_e, h)  # learning information from higher level
+        aggred_mat = torch.mm(soft_e, h)  # 来自上层的矩阵约束
 
         attn_msk, embed_neigh = [], []
         if embed_up is not None:
@@ -97,10 +111,15 @@ class Hierarchy_Embedding(nn.Module):
         if len(attn_msk) != 0:
             attn_msk = torch.cat(attn_msk, dim=1)  # shape: N2*(N1+N2+N3)
             embed_neigh = torch.cat(embed_neigh, dim=0)  # shape: (N1+N2+N3)*F
-            attn_mat, attn_weights = self.multihead_attn[layer](h, embed_neigh, embed_neigh, attn_mask=attn_msk != 1)
+            attn_mat, attn_weights = self.multihead_attn[layer](h.unsqueeze(dim=0),
+                                                                embed_neigh.unsqueeze(dim=0),
+                                                                embed_neigh.unsqueeze(dim=0),
+                                                                attn_mask=attn_msk != 1)
+            attn_mat = attn_mat.squeeze(dim=0)
         else:
             attn_mat = aggred_mat.new_zeros(h.shape)
-
+        # return h
+        # return aggred_mat + 0.1 * attn_mat
         return self.stack_layer[layer](torch.stack([aggred_mat, h, attn_mat], dim=-1)).squeeze(dim=-1)
 
 
@@ -119,9 +138,11 @@ class Hierarchy_Embedding(nn.Module):
         B, L, _ = inputs.shape
         masks = torch.unsqueeze(masks, 1)  # N, 1, L
 
+        #
         top_Rawadp = [self.filterEdge(
             torch.softmax(torch.relu(torch.matmul(self.NodeEmb[i].weight, self.NodeEmb[i].weight.t())), dim=1))
             for i in range(len(self.numbers))]
+        # add_adps = [mat if i == 0 else mat * 0.1 + self.clusters[i - 1] for i, mat in enumerate(top_Rawadp)]
         add_adps = [mat if i == 0 else self.clusters[i - 1] for i, mat in enumerate(top_Rawadp)]
         raw_emb = [self.NodeEmb[i].weight for i in range(len(self.numbers))]
 
@@ -135,19 +156,22 @@ class Hierarchy_Embedding(nn.Module):
                                           hierarchy_high=self.hierarchy_tree[conv_l - 1] if conv_l > 0 else None,
                                           hierarchy_low=self.hierarchy_tree[
                                               conv_l] if conv_l + 1 < len(self.numbers) else None,
-                                          msk_mechanism=conv_l != 0,
+                                          msk_mechanism=conv_l != 0,  # todo:只用第三层，且矩阵为自学习
                                           layer=conv_l)
                 new_embs.append(new_emb)
             raw_emb = new_embs
-
+        #
         reflect_one2two = torch.mm(self.hierarchy_tree[0].t(), raw_emb[0])
         reflect_one2thr = torch.mm(self.hierarchy_tree[1].t(), reflect_one2two)
+
         reflect_two2thr = torch.mm(self.hierarchy_tree[1].t(), raw_emb[1])
 
         features = self.fused_linear(
             self.dropout(torch.stack([reflect_one2thr, reflect_two2thr, raw_emb[-1]], dim=-1))).squeeze(dim=-1)
+        # features = raw_emb[-1]
         attention = (torch.matmul(inputs, features.transpose(0, 1)).transpose(1, 2)).masked_fill(~masks, -np.inf)
         attention = F.softmax(attention, -1)
         attn_out = torch.matmul(attention, inputs)  # N, labels_num, hidden_size
 
         return features, attn_out
+
